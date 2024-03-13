b0abcad8d831e1f34a49e741e3c23e0b05a2fc7e
sshandrenko <123565275+sshandrenko@users.noreply.github.com>
2024-03-13 12:34:23 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('welcomeBtn').addEventListener('click', function() {
+    document.getElementById('welcomeMessage').textContent = 'Hello, and welcome to our amazing web application!';
+});
